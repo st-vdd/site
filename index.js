dafaa9d1491a3be0e55e0a25b4047c950c5260e7
st-vdd <104141726+st-vdd@users.noreply.github.com>
2022-04-21 14:52:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,19 +12,24 @@ function getPage(week, day) {
     let m = document.getElementById('mouth').value;
     // alert("Mouth: "+m+", Week: "+week+", Day: "+day);
     let res = document.getElementById('res');
+    let res1 = document.getElementById('res1');
     let id = 'cont-dayFPC';
     if (window.innerWidth > window.innerHeight) { id = 'cont-dayFPC' } else { id = 'cont-dayFAND' }
     document.getElementById(id).classList.add('none');
     document.getElementById('back').classList.remove('none');
     res.classList.remove('none');
+    res1.classList.remove('none');
     res.src = `./res/${m}/${week}/${day}.jpg`;
+    res1.src = `./res/${m}/${week}/${day}.1.jpg`;
 }
 
 function back() {
     let res = document.getElementById('res');
+    let res1 = document.getElementById('res1');
     let id = 'cont-dayFPC';
     if (window.innerWidth > window.innerHeight) { id = 'cont-dayFPC' } else { id = 'cont-dayFAND' }
     document.getElementById(id).classList.remove('none');
     res.classList.add('none');
+    res1.classList.add('none');
     document.getElementById('back').classList.add('none');
 }
