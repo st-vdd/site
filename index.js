0fd7de2a29760604bfe1a8dddf1d7d0d01010d07
st-vdd <104141726+st-vdd@users.noreply.github.com>
2022-04-21 14:54:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,14 +13,17 @@ function getPage(week, day) {
     // alert("Mouth: "+m+", Week: "+week+", Day: "+day);
     let res = document.getElementById('res');
     let res1 = document.getElementById('res1');
+    let res2 = document.getElementById('res2');
     let id = 'cont-dayFPC';
     if (window.innerWidth > window.innerHeight) { id = 'cont-dayFPC' } else { id = 'cont-dayFAND' }
     document.getElementById(id).classList.add('none');
     document.getElementById('back').classList.remove('none');
     res.classList.remove('none');
     res1.classList.remove('none');
+    res2.classList.remove('none');
     res.src = `./res/${m}/${week}/${day}.jpg`;
     res1.src = `./res/${m}/${week}/${day}.1.jpg`;
+    res2.src = `./res/${m}/${week}/${day}.2.jpg`;
 }
 
 function back() {
@@ -31,5 +34,6 @@ function back() {
     document.getElementById(id).classList.remove('none');
     res.classList.add('none');
     res1.classList.add('none');
+    res2.classList.add('none');
     document.getElementById('back').classList.add('none');
 }
