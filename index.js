95bf4dd3cbfa421d9e57e447a3f21636c1170a0a
st-vdd <104141726+st-vdd@users.noreply.github.com>
2022-04-21 15:04:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -22,13 +22,16 @@ function getPage(week, day) {
     res1.classList.remove('none');
     res2.classList.remove('none');
     res.src = `./res/${m}/${week}/${day}.jpg`;
+    try {
     res1.src = `./res/${m}/${week}/${day}.1.jpg`;
     res2.src = `./res/${m}/${week}/${day}.2.jpg`;
+    }
 }
 
 function back() {
     let res = document.getElementById('res');
     let res1 = document.getElementById('res1');
+    let res2 = document.getElementById('res2');
     let id = 'cont-dayFPC';
     if (window.innerWidth > window.innerHeight) { id = 'cont-dayFPC' } else { id = 'cont-dayFAND' }
     document.getElementById(id).classList.remove('none');
